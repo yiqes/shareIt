3e50f70128b5ea7ec73293e834bdcf6af517e921
yiqes <155768368+yiqes@users.noreply.github.com>
2024-10-21 15:02:23 +0300

    погонка под postman, исправлена ошибка, возникающая при обновлении пользователя с существующим email

--- src/main/java/ru/practicum/user/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/user/UserRepositoryImpl.java
@@ -33,17 +33,23 @@ public class UserRepositoryImpl implements UserRepository {
                 .filter(u -> u.getId().equals(id)) // Ищем по переданному id
                 .findFirst();
 
-        if (existedUser.isPresent()) {
-            if ((user.getEmail() != null && !user.getEmail().isEmpty())  &&
-                    users.stream().anyMatch(u -> u.getEmail().equals(user.getEmail()) && !u.getId().equals(id))) {
+        if (existedUser .isPresent()) {
+            // Проверка на существование email
+            String email = user.getEmail();
+            if (email != null &&
+                    users.stream().anyMatch(u -> email.equals(u.getEmail()) && !u.getId().equals(id))) {
                 throw new ValidationException("Пользователь с таким email уже существует");
             }
-            User userToUpdate = existedUser.get();
-            userToUpdate.setName(user.getName());
-            userToUpdate.setEmail(user.getEmail()); // Обновляем email
+            User userToUpdate = existedUser .get();
+            if (user.getName() != null) {
+                userToUpdate.setName(user.getName());
+            }
+            if (email != null) {
+                userToUpdate.setEmail(email);
+            }
             return userToUpdate;
         } else {
-            throw new NotFoundException("Пользоватлеь не найден");
+            throw new NotFoundException("Пользователь не найден");
         }
     }
     @Override
@@ -77,36 +83,9 @@ public class UserRepositoryImpl implements UserRepository {
         if (email == null || !(email.contains("@"))) {
             throw new ValidationException("Неверно задан email пользователя");
         }
-        if (users.stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
+        // Проверка на существование email
+        if (users.stream().anyMatch(u -> email.equals(u.getEmail()) && !u.getId().equals(user.getId()))) {
             throw new ValidationException("Пользователь с таким email уже существует");
         }
-
     }
 }
-/*public boolean isUserEmailValid(String email) {
-        return users.stream()
-                .filter(user -> user.getEmail() != null) // Проверка на null
-                .anyMatch(user -> user.getEmail().equals(email));
-    }*/
-    /*private void isEmailNew(User user) {
-        if (users.stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
-            throw new ValidationException("Пользователь с таким email уже существует");
-        }
-    }*/
-/*@Override
-    public User updateUser(User user) {
-        isUserValid(user);
-        User existedUser = users.get(Math.toIntExact(user.getId()));
-        existedUser.setEmail(user.getEmail());
-        existedUser.setName(user.getName());
-        return existedUser;
-    }*/
-
-  /*@Override
-    public User updateUser(User user) {
-        isUserValid(user);
-        User existedUser = users.get(Math.toIntExact(user.getId()));
-        existedUser.setEmail(user.getEmail());
-        existedUser.setName(user.getName());
-        return existedUser;
-    }*/
